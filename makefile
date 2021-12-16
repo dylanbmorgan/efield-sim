4193bb6f10eab6126301e43c00648feadcbcb6c9
ShufflerBardOnTheEdge <matyas.parrag@gmail.com>
2021-12-16 14:08:17 +0000

    Finished off writing NetCDF

--- makefile
+++ makefile
@@ -3,5 +3,9 @@ make:
 test_gs:
 	gfortran gauss-seidel_errors.f90 gauss_seidel.f90 gs_test.f90 -o test_gs
 	./test_gs
+test_nc:
+	gfortran write_netcdf.f90 test_nc.f90 -o test_nc `nf-config --fflags --flibs`
+	./test_nc
 clean :
 	 -rm test_gs
+	 -rm test_nc
--- write_netcdf.f90
+++ write_netcdf.f90
@@ -3,43 +3,40 @@ MODULE write_netcdf
     USE netcdf
     IMPLICIT NONE
 
-    ! This type contains all the parameters input by the user when they run the 
+    ! This type contains all the parameters input by the user when they run the
     ! game of life program. It also contains the name of the program so that the
-    ! netcdf file can be traced to a source. 
-    TYPE game_info
-        CHARACTER(20) :: program_name
-    END TYPE
+    ! netcdf file can be traced to a source.
 
     CONTAINS
 
         ! This is the subroutine to write to the netcdf file "filename".
         ! Large amounts of this subroutine were taken from the example given in Workshop 7.
-        SUBROUTINE write(phi, rho, r, v, a, filename, ierr, input_game_info)
+        SUBROUTINE write_array(phi, rho, E_x, E_y, r, v, a, filename)
             ! The final frame of the world array
-            REAL(REAL64), INTENT(IN), DIMENSION(:,:) :: phi, rho
+            REAL(REAL64), INTENT(IN), DIMENSION(:,:) :: phi, rho, E_x, E_y
             REAL(REAL64), INTENT(IN), DIMENSION(:) :: r, v, a
-            ! The game_info metadata
-            TYPE(game_info), INTENT(IN) :: input_game_info
 
-            INTEGER, DIMENSION(2) :: phi_shape
-            INTEGER :: r_shape
             ! The number of dimensions to include in the netcdf file
             INTEGER, PARAMETER :: ndims = 11
             ! Arrays to store the sizes and ids of the dimensions.
             INTEGER, DIMENSION(ndims) :: sizes, dim_ids
             ! The names of the dimensions themselves
-            CHARACTER(1), DIMENSION(ndims) :: dims = ['phi_x', 'phi_y', 'rho_x', 'rho_y', 'E_x_x', 'E_x_y', 'E_y_x', 'E_y_y', 'r', 'v', 'a']
+            CHARACTER(5), DIMENSION(ndims) :: dims = ['phi_x', 'phi_y', 'rho_x', 'rho_y', 'E_x_x', &
+             'E_x_y', 'E_y_x', 'E_y_y', 'r    ', 'v    ', 'a    ']
             ! The filename for the netcdf
             CHARACTER(*), INTENT(IN) :: filename
             ! Error code varaibles and ids
-            INTEGER :: ierr, file_id, phi_var_id, rho_var_id, E_x_var_id, E_y_var_id, r_var_id, v_var_id, a_var_id, i
+            INTEGER :: ierr, file_id, phi_var_id, rho_var_id, E_x_var_id, E_y_var_id, r_var_id, &
+            v_var_id, a_var_id, i
 
             ! Get the sizes of the dimensions in the netcdf file
-            phi_shape = SHAPE(phi)
-            r_shape = SHAPE(r)
-            sizes = [phi_shape(1), phi_shape(2), r_shape]
-
-
+            sizes(1:2) = SHAPE(phi)
+            sizes(3:4) = SHAPE(rho)
+            sizes(5:6) = SHAPE(E_x)
+            sizes(7:8) = SHAPE(E_y)
+            sizes(9) = SIZE(r)
+            sizes(10) = SIZE(v)
+            sizes(11) = SIZE(a)
             ! NOW ENTERING THE CODE FROM WORKSHOP 7 (MY COMMENTS FROM NOW ON WILL BE IN CAPS):
 
             ! Create the file, overwriting if it exists
@@ -57,7 +54,7 @@ MODULE write_netcdf
             ! dimensions, this would really help!
 
             DO i = 1, ndims
-                ierr = nf90_def_dim(file_id, dims(i), sizes(i), dim_ids(i))
+                ierr = nf90_def_dim(file_id, TRIM(dims(i)), sizes(i), dim_ids(i))
                 IF (ierr /= nf90_noerr) THEN
                     PRINT*, TRIM(nf90_strerror(ierr))
                     RETURN
@@ -108,13 +105,6 @@ MODULE write_netcdf
                 RETURN
             END IF
 
-            ! DEFINE THE USER INPUT METADATA FROM THE INPUT_GAME_INFO GAME_INFO TYPE DUMMY VARAIBLE
-            ierr = nf90_put_att(file_id, NF90_GLOBAL, "program_name", input_game_info%program_name)
-            IF (ierr /= nf90_noerr) THEN
-                PRINT*, TRIM(nf90_strerror(ierr))
-                RETURN
-            END IF
-
             ! Finish defining metadata
             ierr = nf90_enddef(file_id)
             IF (ierr /= nf90_noerr) THEN
@@ -124,17 +114,48 @@ MODULE write_netcdf
 
 
             ! Actually write the variable
-            ierr = nf90_put_var(file_id, inarr_var_id, inarr)
+            ierr = nf90_put_var(file_id, phi_var_id, phi)
             IF (ierr /= nf90_noerr) THEN
                 PRINT*, TRIM(nf90_strerror(ierr))
                 RETURN
             END IF
 
-            ierr = nf90_put_var(file_id, time_hist_var_id, time_hist)
+            ierr = nf90_put_var(file_id, rho_var_id, rho)
             IF (ierr /= nf90_noerr) THEN
                 PRINT*, TRIM(nf90_strerror(ierr))
                 RETURN
             END IF
+
+            ierr = nf90_put_var(file_id, E_x_var_id, E_x)
+            IF (ierr /= nf90_noerr) THEN
+                PRINT*, TRIM(nf90_strerror(ierr))
+                RETURN
+            END IF
+
+            ierr = nf90_put_var(file_id, E_y_var_id, E_y)
+            IF (ierr /= nf90_noerr) THEN
+                PRINT*, TRIM(nf90_strerror(ierr))
+                RETURN
+            END IF
+
+            ierr = nf90_put_var(file_id, r_var_id, r)
+            IF (ierr /= nf90_noerr) THEN
+                PRINT*, TRIM(nf90_strerror(ierr))
+                RETURN
+            END IF
+
+            ierr = nf90_put_var(file_id, v_var_id, v)
+            IF (ierr /= nf90_noerr) THEN
+                PRINT*, TRIM(nf90_strerror(ierr))
+                RETURN
+            END IF
+
+            ierr = nf90_put_var(file_id, a_var_id, a)
+            IF (ierr /= nf90_noerr) THEN
+                PRINT*, TRIM(nf90_strerror(ierr))
+                RETURN
+            END IF
+
 
             ! Close the file
             ierr = nf90_close(file_id)
@@ -142,6 +163,6 @@ MODULE write_netcdf
                 PRINT*, TRIM(nf90_strerror(ierr))
                 RETURN
             END IF
-        
-        END SUBROUTINE write
+
+        END SUBROUTINE write_array
 END MODULE write_netcdf
